712099b03ea856638669557ef91515065ab60ccc
Achatac <achatac@unjbg.edu.pe>
2024-07-29 17:18:02 -0500

    Declaracion de los arreglos de registros USUARIO, DISPOSICION, TIENDA

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,42 @@
 //main.cpp 
+
+#include <iostream>
+#include <string>
+#include "panel.h"
+#include "gestion.h"
+#include "mantenimiento.h"
+#include "tienda.h"
+#include "reportes.h"
+#include "clientes.h"
+
+using namespace std;
+
+struct USUARIO {
+    string Nombre;
+    int edad;
+    int numero;
+    int DNI;
+};
+
+USUARIO usuario[100];
+
+struct DISPOSICION {
+    string habitaciones;
+    string categoria;
+    int pisos;
+};
+
+DISPOSICION disposicion[100];
+
+struct TIENDA {
+    string nombreProducto;
+    string descripcion;
+    float precio;
+};
+
+TIENDA tienda[100];
+
+int main (){
+
+    return 0;
+}
