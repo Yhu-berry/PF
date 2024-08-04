f9b9b6126eb992362e7de262cdec560727f1cab8
paduvireh <paduvireh@unjbg.edu.pe>
2024-08-03 23:24:47 -0500

    mantenimiento.cpp: ingresar numero de habitaciones por piso

--- src/mantenimiento.cpp
+++ src/mantenimiento.cpp
@@ -2,15 +2,22 @@
 #include <iostream>
 using namespace std;
 
+struct piso{
+	int nHabitaciones;
+	string categoriasHabitaciones[20];	
+};
+
 int nCategorias=0;
 string CAT[20];
 int nPisos=0;
+
+piso P[20];
 
 void iniciar(){
 	string categoria;
 
 	system("cls");
-    cout<<"======================= INICIO ======================="<<endl<<endl;
+	cout<<"======================= INICIO ======================="<<endl<<endl;
 	cout<<"Complete los siguientes datos:"<<endl<<endl;
 	
 	// ingresar numero de pisos	
@@ -21,9 +28,9 @@ void iniciar(){
 	cin.ignore();
 	cout<<endl;
 	
-    for(int i=0; i<nCategorias; i++){
-        cout<<"   Categoria #"<<i+1<<": "; getline(cin, categoria);
-        CAT[i]=categoria;
+	for(int i=0; i<nCategorias; i++){
+		cout<<"   Categoria #"<<i+1<<": "; getline(cin, categoria);
+		CAT[i]=categoria;
 	}
 	cout<<endl;
     
@@ -36,6 +43,11 @@ void iniciar(){
     cout<<endl<<"------------------------------------------------------"<<endl<<endl;
 	   
 	// ingresar numero de habitaciones por piso
+    for(int i=0; i<nPisos; i++){
+        cout<<"Numero de habitaciones en el piso #"<<i+1<<": "; cin>>P[i].nHabitaciones;
+        cin.ignore();
+	}
+	
 	// asignar categoria a cada habitacion
 	// mostrar categorias asignadas a las habitaciones
     
