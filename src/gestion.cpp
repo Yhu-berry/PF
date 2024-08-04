45a5e71a7a0e49b4bc4c19e00522be0a34515d44
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-08-04 15:21:15 -0500

    reservar_Habitacion: Solicita DNI para validar si tiene una habitacion

--- src/gestion.cpp
+++ src/gestion.cpp
@@ -4,17 +4,24 @@
 #include <string>
 
 using namespace std;
+
 //Variables globales
 USUARIO usuarios[100];
 int TotalUsuarios=0;
 
 //Codigo de las funciones 
 void registrar_usuario(USUARIO& usuario){
+    if(TotalUsuarios>=100){
+        cout<<"Error";
+        return;
+    }
+
     cout<<"Nombre: ";
     cin.ignore();
     getline(cin,usuario.Nombre); //Desde la entrada va a leer el nombre completo
     cout<<"DNI: ";
-    cin>>usuario.DNI;
+    cin.ignore();
+    getline(cin,usuario.DNI);
     cout<<"Edad: ";
     cin>>usuario.edad;
 
@@ -22,3 +29,22 @@ void registrar_usuario(USUARIO& usuario){
     TotalUsuarios++;
 }
 
+//Mostrar habitaciones disponibles del panel.h
+
+//Reservar habitacione
+void reservarHabitacion(){
+    string DNI;
+    cout<<"Ingrese su DNI: ";
+    cin>>DNI;
+
+    for(int i=0; i<TotalUsuarios;i++){
+        if(usuarios[i].DNI==DNI){
+            if(usuarios[i].num_Habitacion==-1){
+                cout<<"Habitacion reservada con exito";
+                //asignar una habitacion en num_Habitacion esto deberia pasarse a panel.cpp
+                return;
+            }
+
+        }
+    }
+}
