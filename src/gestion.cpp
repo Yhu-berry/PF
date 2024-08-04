b053d3c17f1a817f460579f247367ab164cbeb02
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-08-03 20:33:05 -0500

    gestion.cpp: Solicitud de datos del usuario

--- src/gestion.cpp
+++ src/gestion.cpp
@@ -1,1 +1,18 @@
 //gestion.cpp 
+#include "gestion.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Codigo de las funciones 
+void registrar_usuario(){
+    Usuario& usuario=usuarios[100];
+    cout<<"Nombre: ";
+    cin>>usuario.nombre;
+    cout<<"DNI: ";
+    cin>>usuario.DNI;
+    cout<<"Edad: ";
+    cin>>usuario.edad;
+}
+
