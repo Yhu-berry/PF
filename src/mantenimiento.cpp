016d33a23a3c1212ee885be88a7e171d7a3c4a3c
paduvireh <paduvireh@unjbg.edu.pe>
2024-08-03 22:31:29 -0500

    mantenimiento.cpp: ingresar numero de pisos y categorias

--- src/mantenimiento.cpp
+++ src/mantenimiento.cpp
@@ -2,15 +2,32 @@
 #include <iostream>
 using namespace std;
 
+int nCategorias=0;
+string CAT[20];
+int nPisos=0;
+
 void iniciar(){
+	string categoria;
 
 	system("cls");
     cout<<"======================= INICIO ======================="<<endl<<endl;
 	cout<<"Complete los siguientes datos:"<<endl<<endl;
 	
 	// ingresar numero de pisos	
+	cout<<" - Numero de pisos: "; cin>>nPisos;
+	
 	// ingresar categorias
-    // mostrar categorias ingresadas
+	cout<<" - Numero de categorias: "; cin>>nCategorias;
+	cin.ignore();
+	cout<<endl;
+	
+    for(int i=0; i<nCategorias; i++){
+        cout<<"   Categoria #"<<i+1<<": "; getline(cin, categoria);
+        CAT[i]=categoria;
+	}
+	cout<<endl;
+    
+	// mostrar categorias ingresadas
     
     // ingresar numero de habitaciones por piso
     // asignar categoria a cada habitacion
