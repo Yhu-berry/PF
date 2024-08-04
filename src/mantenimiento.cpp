da45414259e9f475f6e153fd6a3344352d60e9ae
paduvireh <paduvireh@unjbg.edu.pe>
2024-08-03 20:22:56 -0500

    mantenimiento.cpp: se agrego estructura base

--- src/mantenimiento.cpp
+++ src/mantenimiento.cpp
@@ -1,1 +1,30 @@
-//mantenimiento.cpp 
+#include "mantenimiento.h"
+#include <iostream>
+using namespace std;
+
+void iniciar(){
+
+	system("cls");
+    cout<<"======================= INICIO ======================="<<endl<<endl;
+	cout<<"Complete los siguientes datos:"<<endl<<endl;
+	
+	// ingresar numero de pisos	
+	// ingresar categorias
+    // mostrar categorias ingresadas
+    
+    // ingresar numero de habitaciones por piso
+    // asignar categoria a cada habitacion
+    // mostrar categorias asignadas a las habitaciones
+    
+    cout<<"======================================================"<<endl<<endl;    
+}
+
+void editar(){
+	// visualizar datos de inicio registrado
+	// agregar pisos -> agregar habitaciones 
+	// agregar categorias -> editar categoria de una habitacion
+}
+
+void visualizar(){
+	// visualizar inicio registrado
+}
