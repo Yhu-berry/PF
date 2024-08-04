a1dfa22ba37e3ccc62caa426a7acb9d8e4082ac7
gpaccip <gpaccip@unjbg.edu.pe>
2024-08-03 22:50:17 -0500

    Añadiendo un menu al panel

--- src/panel.cpp
+++ src/panel.cpp
@@ -4,12 +4,14 @@
 #include "panel.h"
 
 using namespace std;
-
-void panel(){
 	
-	
-	
-	
-	
-	
+void menu() {
+    cout <<"1. ver habitaciones disponibles\n";
+    cout <<"2. reservar habitacion\n";
+    cout <<"3. Mostrar total de habitaciones\n";
+    cout <<"Digite una opcion: "; cin >> op;
+    menuinter();
+    while (salir == 0) {
+        menu();
+	}	
 }
