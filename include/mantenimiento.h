d0e5b83dea2e97eb604294cbb40949971ae9e88c
paduvireh <paduvireh@unjbg.edu.pe>
2024-08-04 00:30:39 -0500

    mantenimiento.h: se agrego struct piso

--- include/mantenimiento.h
+++ include/mantenimiento.h
@@ -1,11 +1,17 @@
 #ifndef MANTENIMIENTO_H
 #define MANTENIMIENTO_H
 
-using namespace std;
+#include <string>
+
+struct piso{
+    int nHabitaciones;
+    std::string categoriasHabitaciones[20];
+};
 
 extern int nCategorias;
-extern string CAT[20];
+extern std::string CAT[20];
 extern int nPisos;
+extern piso P[20];
 
 void iniciar();
 void editar();
