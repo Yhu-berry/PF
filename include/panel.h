2607bec5e49f45ff384403a73c8fb4d4cd02a5ab
gpaccip <gpaccip@unjbg.edu.pe>
2024-08-04 00:23:51 -0500

    Reduciendo las funciones a una donde esta todo

--- include/panel.h
+++ include/panel.h
@@ -2,10 +2,7 @@
 #ifndef PANEL_H
 #define PANEL_H
 
-void menu();
-void menuinter();
-void interfaz();
-void mostrarTotalHabitaciones();
-int nPisos, nHabitaciones, hotel[100][100], z=0, y=1, ocupados[100][100], po, ho, op, salir=0;
+void panel();
+int nPisos, nHabitaciones, hotel[100][100], z = 0, y = 1, ocupados[100][100], po, ho, op, salir = 0;
 
 #endif
