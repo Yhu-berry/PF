853600b506475f18439f27da933e74cfd486a5c6
Achatac <achatac@unjbg.edu.pe>
2024-08-04 11:10:14 -0500

    se implemento la funcion mostrarInterfaz en tienda.cpp

--- src/tienda.cpp
+++ src/tienda.cpp
@@ -1,1 +1,33 @@
-//store.cpp
+//tienda.cpp
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include "tienda.h"
+
+void mostrarInterfaz(){
+
+    do {
+        int opcionTienda;
+        cout << " ----TIENDA ----" << endl;
+        cout << "1.   Verder Producto" << endl;
+        cout << "2.   Mostrar Productos" << endl;
+        cout << "0.   Volver al inicio" << endl;
+        cout << "Seleccione una opcion"; cin >> opcionTienda;
+    
+
+        switch (opcionTienda){
+            case 1: 
+                venderProductos();
+            break;
+            case 2: 
+                mostraProducto();
+            break;
+            case 0: 
+                cout << "Saliendo . . . " << endl;
+            break;
+            default:
+                cout << "Opcion no valida" << endl;
+        }
+    } while (opcionTienda != 0);
+}
