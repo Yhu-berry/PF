94fee9e99d46ecc030b9bc4acae601012d2ee47b
Achatac <achatac@unjbg.edu.pe>
2024-08-04 10:32:13 -0500

    se anadio una estructura de opciones para la interfaz inicial

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,49 @@
 
 using namespace std;
 
-int main (){
+void mostrarInicio(){
+    do {
+        cout << "bienvenido"
+        cout << "1.   Panel" << endl;
+        cout << "2.   Gestion" << endl;
+        cout << "3.   Mantenimiento" << endl;
+        cout << "4.   Tienda" << endl;
+        cout << "5.   Reportes" << endl;
+        cout << "6.   Clientes" << endl;
+        cout << "0.   Salir" << endl;
+        cout << "Seleccione una opcion..." << endl;
 
+        cin >> opcion;
+        switch (opcion){
+            case 1:
+                panel();
+            break;
+            case 2:
+                gestion();
+            break;
+            case 3:
+                mantenimiento();
+            break;
+            case 4:
+                tienda();
+            break;
+            case 5:
+                reportes();
+            breakl;
+            case 6: 
+                clientes();
+            break;
+            case 0:
+                cout << "Saliendo . . . " << endl;
+            break;
+            default:
+            cout << "opcion no valida" << endl;
+
+        }
+    }
+}
+
+int main (){
+    mostrarInicio();
     return 0;
 }
