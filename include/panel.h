023b8d7564b552a2f15e3728e747e4a17af7ba80
gpaccip <gpaccip@unjbg.edu.pe>
2024-08-04 00:21:01 -0500

    Añadiendo variables globales al panel

--- include/panel.h
+++ include/panel.h
@@ -6,5 +6,6 @@ void menu();
 void menuinter();
 void interfaz();
 void mostrarTotalHabitaciones();
-	
+int nPisos, nHabitaciones, hotel[100][100], z=0, y=1, ocupados[100][100], po, ho, op, salir=0;
+
 #endif
