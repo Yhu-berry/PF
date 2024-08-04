6d20727e5cc0a55515e3bbc6557d578a04001920
Yhu-berry <ynmaqueraf@unjbg.edu.pe>
2024-08-04 14:15:49 -0500

    gestion.cpp: Añadiendo la funcion getline()

--- src/gestion.cpp
+++ src/gestion.cpp
@@ -4,15 +4,21 @@
 #include <string>
 
 using namespace std;
+//Variables globales
+USUARIO usuarios[100];
+int TotalUsuarios=0;
 
 //Codigo de las funciones 
-void registrar_usuario(){
-    Usuario& usuario=usuarios[100];
+void registrar_usuario(USUARIO& usuario){
     cout<<"Nombre: ";
-    cin>>usuario.nombre;
+    cin.ignore();
+    getline(cin,usuario.Nombre); //Desde la entrada va a leer el nombre completo
     cout<<"DNI: ";
     cin>>usuario.DNI;
     cout<<"Edad: ";
     cin>>usuario.edad;
+
+    usuario.num_Habitacion=-1;
+    TotalUsuarios++;
 }
 
