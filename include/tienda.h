ce99f34a72dc7137e6838ecb85500d2e2f06ea74
Achatac <achatac@unjbg.edu.pe>
2024-08-04 23:57:44 -0500

    se anadio la estructura tienda y las variables globales

--- include/tienda.h
+++ include/tienda.h
@@ -3,6 +3,16 @@
 #ifndef TIENDA_H
 #define TIENDA_H
 
+#define MAX_PRODUCTOS 100
+
+struct Tienda {
+    std::string nombre;
+    float precio;
+};
+
+extern Producto menu[MAX_PRODUCTOS];
+extern int cantidadProductos;
+
 void mostrarInterfaz();
 void venderProductos();
 void mostrarProductos();
