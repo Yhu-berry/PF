f711e43e6e9db1704bcdfee893d42fce69417314
gpaccip <gpaccip@unjbg.edu.pe>
2024-08-04 20:35:13 -0500

    Quitando la opcion de reservar porque no es necesaria

--- src/panel.cpp
+++ src/panel.cpp
@@ -65,8 +65,7 @@ void mostrarTotalHabitaciones() {
 
 void menu() {
     cout <<"1. ver habitaciones disponibles\n";
-    cout <<"2. reservar habitacion\n";
-    cout <<"3. Mostrar total de habitaciones\n";
+    cout <<"2. Mostrar total de habitaciones\n";
     cout <<"Digite una opcion: "; cin >> op;
     menuinter();
     while (salir == 0) {
