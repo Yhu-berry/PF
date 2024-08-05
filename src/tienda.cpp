2152f7c02c760ce1d5fb05ac14946aeabf52c2fc
gpaccip <gpaccip@unjbg.edu.pe>
2024-08-04 23:17:27 -0500

    Colocando contenido em la funcion mostrarInterfaz

--- src/tienda.cpp
+++ src/tienda.cpp
@@ -5,8 +5,22 @@
 #include <vector>
 #include "tienda.h"
 
-void mostrarInterfaz(){
-
+void mostrarInterfaz() {
+    Producto menu[MAX_PRODUCTOS];
+    int cantidadProductos = 0;
+    int opcion;
+    bool exit = false;
+    
+    while (!exit) {
+        system("cls");
+        cout <<"\tRecepcion-Cafeteria\n\n";
+        cout <<"1. Anadir Producto\n";
+        cout <<"2. Eliminar Producto\n";
+        cout <<"3. Mostrar Productos\n";
+        cout <<"4. Vender Productos\n";
+        cout <<"5. Salir\n";
+        cout <<"Opcion: "; cin >> opcion;
+        cin.ignore();
 
 void mostrarProducto(){
 
