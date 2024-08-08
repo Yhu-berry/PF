824cf48a698af9eb724f601edcdfa349c39a3c62
Achatac <achatac@unjbg.edu.pe>
2024-08-07 22:18:18 -0500

    se complemento las variables globales en estructura.cpp

--- src/estructura.cpp
+++ src/estructura.cpp
@@ -1,16 +1,38 @@
 #include "estructura.h"
 //Inicializacion de las variables
 
+//reportes
 Report factura[100];
-int facturados=0;
-int facturadosGestion=0;
+int facturados = 0;
+int facturadosGestion = 0;
 
+//cafeteria
 Producto menu[MAX_PRODUCTOS];
-int cantidadProductos=0;
-Producto seleccionado[MAX_PRODUCTOS];
+int cantidadProductos = 0;
+Producto seleccion[MAX_PRODUCTOS];
 
-//Gestion
+
+//gestion
 USUARIO usuarios[100];
-int TotalUsuarios=0;
+int TotalUsuarios = 0;
+
+//mantenimiento
+std::string CAT[20];
+int nCategorias = 0;
+std::string CATEGORIA[20];
+float PRECIO[20];
+int nPisos = 0;
+Piso PISO[20];
+
+//panel
+int hotel[100][100] = {0};
+int ocupados[100][100] = {0};
+int po = 0;
+int ho = 0;
+int op = 0;
+int salir = 0;
+
+int posicionUsuarios = 0;
+std::string dni;
 
 //Lo que falta 
